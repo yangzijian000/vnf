25e727e6373c07d22eee3825e03047166c76bce5
Unknown <15979180165@163.com>
2017-09-04 00:00:46 +0800

    9/4
    9/4

---
+++ vnf_c++/vnf_electic.cpp
@@ -0,0 +1,12 @@
+#include "VNF.H"
+
+
+void Loaddata(std::vector<Server> &nodelist,std::vector<Traffic> &trafficlist,std::vector<std::map<int,std::vector<int>>> &k_link){
+	FILE
+}
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
